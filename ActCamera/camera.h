d4bb66601c558082ccc74d61e3bd3a11564968c1
bob <mechanic_liang@126.com>
2017-10-02 15:11:41 +0800

    10月2日下午参数

--- ActCamera/camera.h
+++ ActCamera/camera.h
@@ -39,6 +39,11 @@ public:
         cv::Mat temp;
         videoCapture >> temp;
         originalImage = temp.clone();
+		for (auto i = 0; i < temp.rows; i++)
+			for (auto j = 0; j < temp.cols; j++)
+			{
+				originalImage.ptr<cv::Vec3b>(i)[j] = temp.ptr<cv::Vec3b>(temp.rows - i - 1)[temp.cols - j - 1];
+			}
     }
 
 	//auto set exposure time and white balance
@@ -65,7 +70,7 @@ public:
 	void showImage() const
 	{
 		//cv::imshow("ORG", originalImage);
-		//cv::imshow("ORGROI", originalImage(ROIRect));
+		cv::imshow("ORGROI", originalImage(ROIRect));
 		//cv::imshow("BSC", basicImage);
 		//cv::imshow("AB", allBallImage);
 		//cv::imshow("AG", allGreenImage);
